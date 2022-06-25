2ed941de0a01c2d09e045055d439b4441ff0f95d
shuumusmade <shuumusmade@gmail.com>
2022-06-25 18:58:35 +0530

    updated

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+##DECLARING
+PROJECT_NAME="Housing-Predictor"
+VERSION= "0.0.1"
+AUHTOR= " Shubham musmade"
+DESCRIPTION=" THIS IS MY FIRST MACHINE LEARNING PROJECT"
+PACKAGES=["housing"]
+
+setup(
+name=PROJECT_NAME,
+version= VERSION,
+auhtor=AUHTOR,
+description=DESCRIPTION,
+packages=PACKAGES,
+
+
+
+)
+
+
+
+
+
